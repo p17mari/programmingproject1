6dfc1275059d50449ae800b2e8711eaa93cdbfd0
p17mari <43947917+p17mari@users.noreply.github.com>
2018-11-14 18:34:33 +0200

    Update code.java

--- code.java
+++ code.java
@@ -9,8 +9,7 @@ public class Contact {
    //γίνεται δήλωση μεταβλητών.
    private String fullname;
    private double phonenumber;
-   private String email; 
-   private boolean entry;
+   private String email;
    }
 //κατασκευάζονται constructor.   
 public Contact(String fullname, String email) {
@@ -38,38 +37,39 @@ public static void setPhonenumber(double phonenumber) {
  public static int setNumberOfContacts() {
     return Contact.contacts;
 }
-protected void finalize() {
-   contacts--;
-}
 //χρησιμοποιήται μέθοδος String to String
     public String toString() {
         return "Contact is named "+this.fullname 
                "Contact's email is" this.email
                 +" and has this phone number " +this.phonenumber
+    }
 //υπολογίζεται η main.
 public static void main(String[] args) {
+   //ορίζεται παράδειγμα 1
     Contact invalid1 = new Contact("Ringas Dimitrios", "riggas-ionio.gr");
     invalid1.setPhonenumber=113513701
     System.out.println(invalid1.toString());
+   //ορίζεται παράδειγμα 2
     Contact invalid2= new Contact("Ringas Dimitrios"," riggas@.ioniogr");
     invalid2.setPhonenumber=223513701
+    System.out.println(invalid2.toString()); 
+   //ορίζονται ορθά στοιχεία 1
+    Contact valid1= new Contact("Ringas Dimitrios","riggas@ionio.gr");
+    valid1.setPhonenumber=6948012345
+    System.out.println(valid1.toString());  
+   //οριζονταί ορθά στοιχεια 2
+    Contact valid2= new Contact("Ringas Dimitrios","riggas@ionio.gr ");
+    valid2.setPhonenumber=2661012345
+    System.out.println(valid2.toString());  
   // Χρησιμόποιειται μέθοδος equals για να συγκρίνουμε τα 
        //Fullname
-        System.out.println(george1.getFullname() == new String("Dimitrios Ringas"));    
-        System.out.println(george1.getFullname().equals( new String("Dimitrios Ringas") )); 
-        System.out.println(george1.getFullname() == "Dimitrios Ringas");
+        System.out.println(invalid1.getFullname() == new String("Dimitrios Ringas"));    
+        System.out.println(invalid1.getFullname().equals( new String("Dimitrios Ringas") )); 
+        System.out.println(invalid1.getFullname() == "Dimitrios Ringas");
         //email  
-        System.out.println(george1get.Email() == new String(""));    
-        System.out.println(george1.getEmail().equals( new String("") ))
-        System.out.println(george1.getEmail() == "");
+        System.out.println(invalid1.get.Email() == new String("riggas-ionio.gr"));    
+        System.out.println(invalid1.getEmail().equals( new String("riggas-ionio.gr") ))
+        System.out.println(invalid1.getEmail() == "riggas-ionio.gr");
+   }
+}
            
-        /*
-Invalid email: riggas-ionio.gr
-Invalid number: 113513701
-Same person? false
-Invalid email: riggas@.ioniogr
-Invalid email: riggas@ioniogr.
-Same person? true
-Equal contact entry? false
-Dimitrios Ringas:       2661012345;     riggas@ionio.gr 
-Ringas Dimitrios:       6948012345;     riggas@ionio.gr
