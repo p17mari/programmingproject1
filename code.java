a2780f3696e29d2c7f55774128169ed06770641f
p17mari <43947917+p17mari@users.noreply.github.com>
2018-12-12 19:22:55 +0200

    Update code.java

--- code.java
+++ code.java
@@ -2,86 +2,95 @@
 Π2017089 
 Αργυρώ Μαριόλη
 14/11/18
-Δεν κατάφερα να περιερίσω τους χαρακτήρες που χρησιμοποιούνται στο email.
 */
-/** Υπολογίζεται η κλάση Contact με να περιέχει data members: 
-fullname: Oνοματεπώνυμo τύπος String, phonenumber: Τηλεφωνικός	 αριθμός	τυπου long,	email: τύπου String.*/
+/** Υπολογίζεται η κλάση Contact με να περιέχει data members: 
+fullname: Oνοματεπώνυμo τύπος String, phonenumber: Τηλεφωνικός	 αριθμός	τυπου long,	email: τύπου String.*/
 public class Contact { 
    //γίνεται δήλωση μεταβλητών.
    private String fullname;
    private long phonenumber;
    private String email;
-   }
+ 
 //κατασκευάζονται constructor.   
+    public Contact(String fullname, String email, long phonenumber) {
+        this(fullname, email);
+        this.phonenumber=phonenumber;
+    }
 
-public Contact(String fullname) {
-   this.fullname=fullname;
-   } 
-public Contact(String email){
-   this.email=email;
-   }
-public Contact (String name,long number) 
+    public Contact (String name,long phonenumber) 
    {
-      fullName = name;
-      this.setPhonenumber(number); //χρήση μεθόδου set.
+      this.fullname = name;
+      this.setPhonenumber(phonenumber); //χρήση μεθόδου set.
    }
-public Contact(String fullname, String email, long phonenumber) {
-   this(fullname, email);
-   this.phonenumber=phonenumber;
+   public Contact (String name,String email) 
+   {
+      this.fullname = name;
+      this.setEmail(email); //χρήση μεθόδου set.
    }
-//χρησιμοποιήται μέθοδος get.
-public String getFullname() {
-   return this.fullname;
-}
-public String getEmail() {
-   return this.email;
-}
-public long getPhonenumber {
-   return this.phonenumber;
-}
+   public Contact(){
+    
+   }
 //χρησιμοποιήται μέθοδος set.
-public static void setPhonenumber(long phonenumber) {
-   this.phonenumber=phonenumber;
-}
- public static int setNumberOfContacts() {
-    return Contact.contacts;
-}
+    public void setPhonenumber(long phonenumber) {
+       this.phonenumber=phonenumber;
+    }
+    public void setEmail(String email) {
+        this.email=email;
+    }
+    public void setFullname(String fullname) {
+        this.fullname=fullname;
+    }
+//χρησιμοποιήται μέθοδος get.
+
+
+    public String getFullname() {
+        return fullname;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+ 
+
+    public long getPhonenumber  () {
+        return phonenumber;
+    }
+
 //χρησιμοποιήται μέθοδος String to String
     public String toString() {
-       contact = fullName + ":\t" + phonenumber + ";\t" + email;
-        return contact
+        return fullname + ":\t" + phonenumber + ";\t" + email;
     }
 //επιστρέφει boolean τιμή που δηλώνει αν το Contact αναπαριστά ίδιο πρόσωπο με το αντικείμενο Contact other.
- public boolean equals(Contact other)
+    public boolean equals(Contact other)
    {
-      return this.fullName==other.fullName && this.phonenumber==other.phonenumber && this.email==other.email;
+      return this.fullname==other.fullname && this.phonenumber==other.phonenumber && this.email==other.email;
    }
 //επιστρέφει boolean τιμή που δηλώνει αν το Contact είναι όμοιο με το αντικείμενο Contact other.
-  public boolean isDuplicate(Contact other)
+    public boolean isDuplicate(Contact other)
    {
       return this.phonenumber == other.phonenumber || this.email == other.email;
    }
 //υπολογίζεται η main.
-public static void main(String[] args) {
-   //ορίζεται παράδειγμα valid1
-    Contact valid1 = new Contact(){
-    invalid1.setEmail=riggas-ionio.gr
-    valid1.setFullname("Dimitrios Ringas");
-    valid1.setEmail("riggas-ionio.gr");
-    valid1.setEmail("riggas@ionio.gr");
-    valid1.setPhonenumber(6948012345));
-    valid1.setPhonenumber(0661341785);
-    System.out.println(valid1.toString());
-    }
-   //ορίζεται παράδειγμα valid2
-   valid2 = new Contact("Ringas Dimitrios", 6948012345);
-   //χρήση μεθόδου isDuplicate για να συγκριθούν μερικά στοιχεία του valid2 με του valid1.
-    System.out.println("Same person? "+ valid2.isDuplicate(valid1) );
-    valid2.setEmail=riggas@.ionio.gr
-    System.out.println(valid2.toString()); 
-   //χρήση μεθόδου equals για να συγκριθούν όλα τα στοιχεία του valid2 με του valid1.
-    System.out.println("Equal contact entry? "+ valid2.equals(valid1) );
-    System.out.println(valid1);
-    System.out.println(valid2);
+    public static void main(String[] args) {
+   //ορίζεται παράδειγμα c1
+    Contact c1 = new Contact();
+    c1.setEmail("riggas-ionio.gr");
+    c1.setFullname("Dimitrios Ringas");
+    c1.setEmail("riggas@ionio.gr");
+    c1.setEmail("riggas-ionio.gr");
+    c1.setPhonenumber(6948012345L);
+    c1.setPhonenumber(2661341785L);
     
+    
+   //ορίζεται παράδειγμα c2
+   Contact c2 = new Contact("Ringas Dimitrios", 6948012345L);
+   //χρήση μεθόδου isDuplicate για να συγκριθούν μερικά στοιχεία του c2 με του c1.
+    System.out.println("Same person? "+ c2.isDuplicate(c1) );
+    c2.setEmail("riggas@.ionio.gr");
+    
+   //χρήση μεθόδου equals για να συγκριθούν όλα τα στοιχεία του c2 με του c1.
+    System.out.println("Equal contact entry? "+ c2.equals(c1) );
+    System.out.println(c1);
+    System.out.println(c2);
    }
+}
